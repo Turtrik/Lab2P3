2df1d6be444d1a644e1d622b2216faf6803b7c43
Stephen <scarey8@uncc.edu>
2021-03-02 17:39:21 -0500

    Implement functionality to totalImpurePrice

--- Invoice.py
+++ Invoice.py
@@ -3,5 +3,11 @@ class Invoice:
     def __init__(self):
         self.items = {}
 
+    #Calculates the price * the quantity for each item in products
+    #returns the total cost for all items
     def totalImpurePrice(self, products):
-        pass
+        total_impure_price = 0
+        for k, v in products.items():
+            total_impure_price += float(v['unit_price']) * int(v['qnt'])
+        total_impure_price = round(total_impure_price, 2)
+        return total_impure_price
