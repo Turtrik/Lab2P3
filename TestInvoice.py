a78c8e9a75fb3027a1fa2afcf50144b2f071fe3c
Stephen <scarey8@uncc.edu>
2021-03-02 17:23:41 -0500

    Failing test case

--- TestInvoice.py
+++ TestInvoice.py
@@ -6,3 +6,6 @@ def products():
     products = {'pen': {'qnt': 10, 'unit_price': 3.75, 'discount': 5},
                 'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
     return products
+
+def test_CanFindInvoiceClass():
+    invoice = Invoice()
